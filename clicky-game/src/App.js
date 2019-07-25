0ce3b9bc53567050e8235724e8595423d52c6bce
Justin German <justin.german23@gmail.com>
2019-07-25 16:10:14 -0500

    added scoreboard for correct and incorrect  score

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
 import MainGame from "./components/MainGame/MainGame";
 const App = () => <MainGame />;
+
 export default App;
---
+++ clicky-game/src/Scoreboard/Scoreboard.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Scoreboard extends Component {
+    state = {
+        message: "",
+        numberScore: false
+    };
+
+    componentWillReceiveProps({ score, highScore }) {
+        let newState = { score: true };
+        if (score === 0 && highScore === 0) {
+            newState.message = "";
+        } else if (score === 0 && highScore > 0) {
+            newState.message = "incorrect";
+        } else {
+            newState.message = "correct";
+        }
+        this.setState(newState, () =>
+            setTimeout(() => this.setState({ numberScore: false }), 600)
+        );
+    }
+
+    renderMessage = () => {
+        switch (this.state.message) {
+            case "correct":
+                return "You Guessed Correctly!";
+            case "incorrect":
+                return "You Guessed Incorrectly!";
+            default:
+                return "Click an image to get started!";
+        }
+    };
+
+    render() {
+        return (
+            <li className={this.state.numberScore ? this.state.message : ""}>
+                {this.renderMessage()}
+            </li>
+        );
+    }
+
+}
+
+export default Scoreboard;
