582f9a031cda3477ea1f508c172a17a4dff09ba9
Justin German <justin.german23@gmail.com>
2019-07-25 23:53:28 -0500

    added container and header to front-end

---
+++ clicky-game/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = props => (
+    <header className="header">
+        <h1>Welcome to The Simpsons Click Game!</h1>
+        <h2>Click an image to earn points, but don't click on any more than once!</h2>
+    </header>
+);
+
+export default Header;
